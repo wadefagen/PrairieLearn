9dbc7a0e329ae3024e9b35f8d7958cc634b23f8b
Wade Fagen <waf@illinois.edu>
2014-09-09 11:20:29 -0500

    Changes currently on edu.cs.illinois.edu

--- frontend/require/AdaptiveTestSidebarView.js
+++ frontend/require/AdaptiveTestSidebarView.js
@@ -5,6 +5,11 @@ define(["underscore", "backbone", "mustache", "AdaptiveTestHelper", "text!Adapti
 
         tagName: 'div',
 
+        events: {
+		"show.bs.modal": "loadVideo",
+		"hide.bs.modal": "unloadVideo"
+	},
+
         initialize: function() {
             this.test = this.options.test;
             this.tInstance = this.options.tInstance;
@@ -12,6 +17,15 @@ define(["underscore", "backbone", "mustache", "AdaptiveTestHelper", "text!Adapti
             this.listenTo(this.test, "change", this.render);
             this.listenTo(this.tInstance, "change", this.render);
         },
+
+        loadVideo: function() {
+	    var $player = $('#player');
+	    $player.attr('src', $player.data('url'));
+	},
+
+        unloadVideo: function() {
+	    $('#player').attr('src', '');
+	},
 
         render: function() {
             var that = this;
@@ -43,6 +57,8 @@ define(["underscore", "backbone", "mustache", "AdaptiveTestHelper", "text!Adapti
                 data.prevQNumber = qIndex;
             if (qIndex < qids.length - 1)
                 data.nextQNumber = qIndex + 2;
+
+            data.video = this.model.get("video");
 
             data.recommendBar = AdaptiveTestHelper.renderRecommendBar(modelData, qid);
             data.correctPoints = AdaptiveTestHelper.renderCorrectPoints(modelData, qid);
